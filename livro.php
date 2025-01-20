54bd173e8f8d1f3c67c38609b9bbd893f14df36e
feelp19 <flp.pietro19@gmail.com>
2025-01-20 13:12:52 -0300

    Add a book page

---
+++ livro.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>BookWise</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-stone-950 text-stone-200">
+<header class="bg-stone-900">
+    <nav class="mx-auto max-w-screen-lg flex items-center justify-between py-4">
+        <div class="font-bold text-xl tracking-wide">Book Wise</div>
+        <ul class="flex items-center justify-between gap-x-2 font-bold">
+            <li><a href="/" class="hover:underline text-lime-500">Explorar</a></li>
+            <li><a href="/meus-livros.php" class="hover:underline">Meus Livros</a></li>
+        </ul>
+
+        <ul>
+            <li><a href="/login.php" class="hover:underline">Fazer Login</a></li>
+        </ul>
+    </nav>
+</header>
+
+<main class="mx-auto max-w-screen-lg space-y-6">
+
+</main>
+</body>
+</html>
