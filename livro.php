5578a5fcc827e3e3a242f398bb1f4dcec27331ec
feelp19 <flp.pietro19@gmail.com>
2025-01-21 15:44:58 -0300

    .

--- livro.php
+++ livro.php
@@ -2,16 +2,22 @@
 
 require 'dados.php';
 
-$id = $_REQUEST['id'];
-$livroFiltrado = array_filter($livros, fn($l) => $l['id'] == $id);
+$id = $_REQUEST['id']; //pega o request que esta sendo informado na view livro, ao clicar
+$livroFiltrado = array_filter($livros, fn($l) => $l['id'] == $id); //filtra os valores do array(dados) onde a condicao, seja que o id seja igual ao que pegamos da request
 
-$livro = array_pop($livroFiltrado);
+$livro = array_pop($livroFiltrado); //pega o primeiro valor do array, referente a condicao atendida
 
-$view = "livro";
+
+$uri = $_SERVER['REQUEST_URI'];
+$uriSemBarra = str_replace('/', '', $uri);
+
+var_dump($uri, $uriSemBarra);
+
+$view = 'livro';
 require "views/template/app.php";
-
-
-?>
 
 //controller, pois esta manipulando os valores para a view exibir
 //pelo array_filter e pelo array_filtrado
+
+?>
+
